119af1cdee189f8398a4d509638e73180f94494d
sotasota7 <75403749+sotasota7@users.noreply.github.com>
2021-07-14 19:55:26 +0900

    Update admin.py

--- taskgame/admin.py
+++ taskgame/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Category, Todo
+
+
+admin.site.register(Category)
+admin.site.register(Todo)
