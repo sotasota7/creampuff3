18e5e9d0e11e2a040fe066f4b8163ce9d7974605
sotasota7 <75403749+sotasota7@users.noreply.github.com>
2021-07-14 19:54:53 +0900

    Update models.py

--- taskgame/models.py
+++ taskgame/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField('タイトル', max_length=20)
+
+    """self.titleとすることでadmin管理画面にてインスタンス変数として表示される"""
+    def __str__(self):
+        return self.title
+
+
+"""
+タイトル、日付テーブルとカテゴリーを紐づけるためのテーブル。
+PROTECTは紐づいているデータが存在すれば消されない
+"""
+class Todo(models.Model):
+    title = models.CharField('タイトル', max_length=50)
+    created_at = models.DateTimeField('日付', auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
